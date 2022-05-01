fac7d4106097f606d370c864e54420e3c1dc9d85
Vtres <viniciust98@outlook.com>
2022-04-30 22:49:18 -0300

    :tada: finalizando guia básico

--- components/Hooks.js
+++ components/Hooks.js
@@ -1,15 +1,29 @@
-import React,{useEffect, useState} from 'react';
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Button, Text, View } from "react-native";
 
-export default function Hooks(){
+export default function Hooks() {
 
-    useEffect(()=>{
+    const [count, setCount] = useState(0)
+    const [lista, setLista] = useState(['Vini', 'Lucas', 'Pedro', 'João'])
+    useEffect(() => {
         alert('Hook de efeito')
-    })
+    }, [])
 
-    return(
-        <View>
-            <Text>Olá</Text>
+    return (
+        <View style={{ padding: 20 }}>
+            <View style={{marginTop:10}}>
+                <Button title='Contando numero' onPress={() => setCount(count + 1)}></Button>
+                <Text>{count}</Text>
+            </View>
+            <View style={{marginTop:10}}>
+            <Button title='Incrementando lista' onPress={() => setLista([...lista,'salada'])}></Button>
+                {
+                    lista.map((item) => {
+                        return (<Text key={item}>{item}</Text>);
+                    })
+                }
+
+            </View>
         </View>
     )
 }
